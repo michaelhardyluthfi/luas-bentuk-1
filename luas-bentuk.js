859489e8fdd8c8a77fb77c70d0d6fd37b301c75f
Adwityosp <adwityosp.gmail.com>
2022-05-30 20:03:24 +0700

    push1

---
+++ luas-bentuk.js
@@ -0,0 +1,37 @@
+/*buat project dengan nama : "luas-bentuk"
+menggunakan node js, dengan tujuan luas sebagai package
+buat methode/function hitung luas bangung :
+a. persegi
+b. lingkaran
+c. segitiga
+d. jajargenjang
+e. persegi panjang
+per orang 1 function
+push code ke github repo dengan nama luas-bentuk
+dengan minimal perorang 1 commit
+pilih seorang untuk membuat repository*/
+let luasbentuk = require("luas-bentuk");
+var namaBangun = prompt('pilih jenis bangun : ');
+if (namaBangun=="luasPersegi"){ 
+    function luasPersegi();
+}
+else if (namaBangun=="luasLingkaran"){
+    function luasLingkaran();
+}
+else if (namaBangun=="luasSegitiga"){
+    function luasSegitiga();
+}
+else if (namaBangun=="luasJajargenjang"){
+    function luasJajargenjang();
+}
+else {
+    function luasPersegipanjang();
+}
+function luasPersegipanjang (){
+    var panjang = prompt('masukan panjang : ');
+    var lebar = prompt('masukan lebar : ');
+    var persegi = panjang * lebar;
+    return persegi;
+}
+
+export{luasPersegipanjang};
